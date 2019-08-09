f99389800dbdfa1d04df26a922ec7ab6d286f302
Hunter Perrin <hperrin@gmail.com>
2019-08-08 19:16:54 -0700

    Oops.

--- components/top-app-bar/DenseFixedAdjust.js
+++ components/top-app-bar/DenseFixedAdjust.js
@@ -1,9 +1,9 @@
-export default function FixedAdjust(node) {
-  node.classList.add('mdc-top-app-bar--fixed-adjust');
+export default function DenseFixedAdjust(node) {
+  node.classList.add('mdc-top-app-bar--dense-fixed-adjust');
 
   return {
     destroy() {
-      node.classList.remove('mdc-top-app-bar--fixed-adjust');
+      node.classList.remove('mdc-top-app-bar--dense-fixed-adjust');
     }
   }
 }
