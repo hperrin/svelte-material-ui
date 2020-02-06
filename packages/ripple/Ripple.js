1f831d9f816a2641ad393842bd8375b0974e29d9
Marcos <mrodriguezma@minsait.com>
2020-02-06 15:44:47 +0100

    Removed console logs in Ripple.js

--- packages/ripple/Ripple.js
+++ packages/ripple/Ripple.js
@@ -14,7 +14,6 @@ export default function Ripple(node, props = {ripple: false, unbounded: false, c
       classList.push(className);
       if (props.classForward) {
         props.classForward(classList);
-        console.log('addClass', className, classList);
       }
     }
   }
@@ -26,7 +25,6 @@ export default function Ripple(node, props = {ripple: false, unbounded: false, c
       classList.splice(idx, 1);
       if (props.classForward) {
         props.classForward(classList);
-        console.log('removeClass', className, classList);
       }
     }
   }
