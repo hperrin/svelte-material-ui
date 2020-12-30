a411f4b8fed2378d0064715192066d017837ead9
Hunter Perrin <hperrin@gmail.com>
2020-12-29 17:59:55 -0800

    chore: create fragment component to inject context

---
+++ packages/common/ContextFragment.svelte
@@ -0,0 +1,18 @@
+<slot></slot>
+
+<script>
+  import {onDestroy, setContext} from 'svelte';
+  import {writable} from 'svelte/store';
+
+  export let key;
+  export let value;
+
+  const storeValue = writable(value);
+  setContext(key, storeValue);
+
+  $: $storeValue = value;
+
+  onDestroy(() => {
+    storeValue.set(undefined);
+  });
+</script>
