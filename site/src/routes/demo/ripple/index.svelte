153f727bb3b721d3532e1b1425ce9cf7bcd6c9a4
Hunter Perrin <hperrin@gmail.com>
2021-04-13 07:47:47 -0700

    docs: move Ripple to new source viewer

--- site/src/routes/demo/ripple/index.svelte
+++ site/src/routes/demo/ripple/index.svelte
@@ -5,136 +5,27 @@
 <section>
   <h2>Ripple</h2>
 
-  <p use:Ripple={{ surface: true }} tabindex="0">
-    SMUI ripples can be added to arbitrary elements, like this <code>p</code>
-    element. Try clicking it to see the ripple.
-  </p>
+  <Simple />
+  <ShowSource file="ripple/Simple.svelte" />
 
-  <p use:Ripple={{ surface: true, color: 'primary' }} tabindex="0">
-    Primary color.
-  </p>
+  <PrimaryColor />
+  <ShowSource file="ripple/PrimaryColor.svelte" />
 
-  <p use:Ripple={{ surface: true, color: 'secondary' }} tabindex="0">
-    Secondary color.
-  </p>
+  <SecondaryColor />
+  <ShowSource file="ripple/SecondaryColor.svelte" />
 
-  <h6 class="demo-title">Unbounded</h6>
-  <p>
-    <span
-      use:Ripple={{
-        surface: true,
-        unbounded: true,
-        // addClass and removeClass need to be provided, since
-        // we have a "class" attribute on this element.
-        addClass: (className) => {
-          if (!unboundedRippleClasses[className]) {
-            unboundedRippleClasses[className] = true;
-          }
-        },
-        removeClass: (className) => {
-          if (unboundedRippleClasses[className]) {
-            delete unboundedRippleClasses[className];
-            unboundedRippleClasses = unboundedRippleClasses;
-          }
-        },
-      }}
-      tabindex="0"
-      class="unbounded {Object.keys(unboundedRippleClasses).join(' ')}"
-    >
-      D
-    </span>
-    <span
-      use:Ripple={{
-        surface: true,
-        unbounded: true,
-        color: 'primary',
-        // addClass and removeClass need to be provided, since
-        // we have a "class" attribute on this element. If we had
-        // a "style" attribute, we would also need addStyle.
-        addClass: (className) => {
-          if (!unboundedPrimaryRippleClasses[className]) {
-            unboundedPrimaryRippleClasses[className] = true;
-          }
-        },
-        removeClass: (className) => {
-          if (unboundedPrimaryRippleClasses[className]) {
-            delete unboundedPrimaryRippleClasses[className];
-            unboundedPrimaryRippleClasses = unboundedPrimaryRippleClasses;
-          }
-        },
-      }}
-      tabindex="0"
-      class="unbounded {Object.keys(unboundedPrimaryRippleClasses).join(' ')}"
-    >
-      P
-    </span>
-    <span
-      use:Ripple={{
-        surface: true,
-        unbounded: true,
-        color: 'secondary',
-        // addClass and removeClass need to be provided, since
-        // we have a "class" attribute on this element. If we had
-        // a "style" attribute, we would also need addStyle.
-        addClass: (className) => {
-          if (!unboundedSecondaryRippleClasses[className]) {
-            unboundedSecondaryRippleClasses[className] = true;
-          }
-        },
-        removeClass: (className) => {
-          if (unboundedSecondaryRippleClasses[className]) {
-            delete unboundedSecondaryRippleClasses[className];
-            unboundedSecondaryRippleClasses = unboundedSecondaryRippleClasses;
-          }
-        },
-      }}
-      tabindex="0"
-      class="unbounded {Object.keys(unboundedSecondaryRippleClasses).join(' ')}"
-    >
-      S
-    </span>
-  </p>
+  <Unbounded />
+  <ShowSource file="ripple/Unbounded.svelte" />
 
-  <p
-    use:Ripple={{ surface: true, active }}
-    on:mousedown|capture={() => (active = true)}
-    on:mouseup|capture={() => (active = false)}
-    on:keydown|capture={(e) =>
-      (active = e.code === 'Space' || e.code === 'Enter')}
-    on:keyup|capture={() => (active = false)}
-    tabindex="0"
-  >
-    Keyboard activation on a non-button non-link element. (Focus and press
-    space/enter.)
-  </p>
+  <KeyboardActivation />
+  <ShowSource file="ripple/KeyboardActivation.svelte" />
 </section>
 
 <script>
-  import Ripple from '@smui/ripple';
-
-  let unboundedRippleClasses = {};
-  let unboundedPrimaryRippleClasses = {};
-  let unboundedSecondaryRippleClasses = {};
-  let active = false;
+  import ShowSource from '../../../components/ShowSource.svelte';
+  import Simple from './Simple.svelte';
+  import PrimaryColor from './PrimaryColor.svelte';
+  import SecondaryColor from './SecondaryColor.svelte';
+  import Unbounded from './Unbounded.svelte';
+  import KeyboardActivation from './KeyboardActivation.svelte';
 </script>
-
-<style>
-  p {
-    padding: 10px;
-    border-radius: 5px;
-  }
-
-  .unbounded {
-    display: inline-flex;
-    width: 50px;
-    height: 50px;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    font-size: 20px;
-  }
-
-  [tabindex='0'] {
-    cursor: pointer;
-  }
-</style>
