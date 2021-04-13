e6d71087d5075354e7b77a55a95072edffb642a6
Hunter Perrin <hperrin@gmail.com>
2021-04-13 08:06:22 -0700

    docs: move Snackbars to new source viewer

---
+++ site/src/routes/demo/snackbars/Simple.svelte
@@ -0,0 +1,26 @@
+<Snackbar bind:this={snackbarWithClose}>
+  <Label>This is a snackbar.</Label>
+  <Actions>
+    <IconButton class="material-icons" title="Dismiss">close</IconButton>
+  </Actions>
+</Snackbar>
+
+<Snackbar bind:this={snackbarWithoutClose}>
+  <Label>This is a snackbar.</Label>
+</Snackbar>
+
+<Button on:click={() => snackbarWithClose.open()}>
+  <Label>Open Snackbar With Dismiss</Label>
+</Button>
+<Button on:click={() => snackbarWithoutClose.open()}>
+  <Label>Open Snackbar Without Dismiss</Label>
+</Button>
+
+<script>
+  import Snackbar, { Actions, Label } from '@smui/snackbar';
+  import Button from '@smui/button';
+  import IconButton from '@smui/icon-button';
+
+  let snackbarWithClose;
+  let snackbarWithoutClose;
+</script>
