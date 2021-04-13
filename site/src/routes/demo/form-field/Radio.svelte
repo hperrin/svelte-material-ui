14f676e62141d8c7568ab69c67fa13552b8cb88f
Hunter Perrin <hperrin@gmail.com>
2021-04-12 20:55:44 -0700

    docs: move Form Field to new source viewer

---
+++ site/src/routes/demo/form-field/Radio.svelte
@@ -0,0 +1,17 @@
+<h6 class="demo-title">Radio Button</h6>
+
+{#each ['yes', 'no'] as option}
+  <FormField style="margin-right: 1em;">
+    <Radio bind:group={selected} value={option} />
+    <span slot="label">{`${option[0].toUpperCase()}${option.slice(1)}`}</span>
+  </FormField>
+{/each}
+
+<pre class="status">Selected: {selected}</pre>
+
+<script>
+  import FormField from '@smui/form-field';
+  import Radio from '@smui/radio';
+
+  let selected = 'yes';
+</script>
--- site/src/routes/demo/form-field/index.svelte
+++ site/src/routes/demo/form-field/index.svelte
@@ -5,53 +5,19 @@
 <section>
   <h2>Form Fields</h2>
 
-  <div>
-    <FormField>
-      <Checkbox bind:checked />
-      <span slot="label"
-        >Form fields let you click the label to toggle or focus the form
-        control.</span
-      >
-    </FormField>
-  </div>
+  <Checkbox />
+  <ShowSource file="form-field/Checkbox.svelte" />
 
-  <pre class="status">Checked: {checked ? 'Yes' : 'No'}</pre>
+  <Radio />
+  <ShowSource file="form-field/Radio.svelte" />
 
-  <div>
-    {#each ['yes', 'no'] as option}
-      <FormField style="margin-right: 1em;">
-        <Radio bind:group={selected} value={option} />
-        <span slot="label"
-          >{`${option[0].toUpperCase()}${option.slice(1)}`}</span
-        >
-      </FormField>
-    {/each}
-  </div>
-
-  <pre class="status">Selected: {selected}</pre>
-
-  <div>
-    <FormField>
-      <Switch bind:checked={agreed} />
-      <span slot="label"
-        >I agree to the terms and conditions of the software, <small
-          style="opacity: .4;"
-          >and hereby sign away my rights just to use this app.</small
-        ></span
-      >
-    </FormField>
-  </div>
-
-  <pre class="status">Agreed: {agreed ? 'Yes, muahahah.' : 'Not yet.'}</pre>
+  <Switch />
+  <ShowSource file="form-field/Switch.svelte" />
 </section>
 
 <script>
-  import FormField from '@smui/form-field';
-  import Radio from '@smui/radio';
-  import Checkbox from '@smui/checkbox';
-  import Switch from '@smui/switch';
-
-  let checked = false;
-  let selected = 'yes';
-  let agreed = false;
+  import ShowSource from '../../../components/ShowSource.svelte';
+  import Checkbox from './Checkbox.svelte';
+  import Radio from './Radio.svelte';
+  import Switch from './Switch.svelte';
 </script>
