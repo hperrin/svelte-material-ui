dd945100dd8060cf39d445bc08da225460ed9899
Hunter Perrin <hperrin@gmail.com>
2021-04-12 21:16:30 -0700

    docs: move Image List to new source viewer

---
+++ site/src/routes/demo/image-list/Masonry.svelte
@@ -0,0 +1,34 @@
+<!--
+  Check out Masonry.scss
+-->
+
+<h6 class="demo-title">Masonry, with rounded shapes</h6>
+
+<ImageList class="my-image-list-masonry" masonry>
+  {#each Array(15) as _unused, i}
+    <Item>
+      <Image
+        src="https://place-hold.it/190x{getUnevenImageSize(
+          i,
+          107,
+          200,
+          Math.abs
+        )}?text=190x{getUnevenImageSize(i, 107, 200, Math.abs)}&fontsize=23"
+        alt="Image {i + 1}"
+      />
+      <Supporting>
+        <Label>Image {i + 1}</Label>
+      </Supporting>
+    </Item>
+  {/each}
+</ImageList>
+
+<script>
+  import ImageList, { Item, Image, Supporting, Label } from '@smui/image-list';
+  import './Masonry.scss';
+
+  function getUnevenImageSize(counter, base, variance, preAdd = (num) => num) {
+    const mid = (counter % 2 ? Math.cos : Math.sin)(counter) * variance;
+    return base + Math.floor(preAdd(mid));
+  }
+</script>
--- site/src/routes/demo/image-list/index.svelte
+++ site/src/routes/demo/image-list/index.svelte
@@ -5,107 +5,23 @@
 <section>
   <h2>Image Lists</h2>
 
-  <div>
-    <ImageList class="my-image-list-standard">
-      {#each Array(15) as _unused, i}
-        <Item>
-          <ImageAspectContainer>
-            <Image
-              src="https://place-hold.it/190x190?text=square&fontsize=23"
-              alt="Image {i + 1}"
-            />
-          </ImageAspectContainer>
-          <Supporting>
-            <Label>Image {i + 1}</Label>
-          </Supporting>
-        </Item>
-      {/each}
-    </ImageList>
-  </div>
+  <Simple />
+  <ShowSource file="image-list/Simple.svelte" />
 
-  <div>
-    <h6 class="demo-title">4x5 aspect ratio, with text protection</h6>
+  <TextProtection />
+  <ShowSource file="image-list/TextProtection.svelte" />
 
-    <ImageList class="my-image-list-4x5" withTextProtection>
-      {#each Array(15) as _unused, i}
-        <Item>
-          <ImageAspectContainer>
-            <Image
-              src="https://place-hold.it/190x238?text=4x5&fontsize=23"
-              alt="Image {i + 1}"
-            />
-          </ImageAspectContainer>
-          <Supporting>
-            <Label>Image {i + 1}</Label>
-          </Supporting>
-        </Item>
-      {/each}
-    </ImageList>
-  </div>
+  <Masonry />
+  <ShowSource file="image-list/Masonry.svelte" />
 
-  <div>
-    <h6 class="demo-title">Masonry, with rounded shapes</h6>
-
-    <ImageList class="my-image-list-masonry" masonry>
-      {#each Array(15) as _unused, i}
-        <Item>
-          <Image
-            src="https://place-hold.it/190x{getUnevenImageSize(
-              i,
-              107,
-              200,
-              Math.abs
-            )}?text=190x{getUnevenImageSize(i, 107, 200, Math.abs)}&fontsize=23"
-            alt="Image {i + 1}"
-          />
-          <Supporting>
-            <Label>Image {i + 1}</Label>
-          </Supporting>
-        </Item>
-      {/each}
-    </ImageList>
-  </div>
-
-  <div>
-    <h6 class="demo-title">
-      Using a <code>div</code> instead of an <code>img</code> to enforce aspect ratio
-    </h6>
-
-    <ImageList class="my-image-list-enforce-ratio">
-      {#each Array(15) as _unused, i}
-        <Item>
-          <ImageAspectContainer>
-            <Image
-              component={Div}
-              style="background-image: url(https://place-hold.it/190x{getUnevenImageSize(
-                i,
-                190,
-                10
-              )}?text=190x{getUnevenImageSize(i, 190, 10)}&fontsize=23);"
-            />
-          </ImageAspectContainer>
-          <Supporting>
-            <Label>Image {i + 1}</Label>
-          </Supporting>
-        </Item>
-      {/each}
-    </ImageList>
-  </div>
+  <EnforceAspectRatio />
+  <ShowSource file="image-list/EnforceAspectRatio.svelte" />
 </section>
 
 <script>
-  import ImageList, {
-    Item,
-    ImageAspectContainer,
-    Image,
-    Supporting,
-    Label,
-  } from '@smui/image-list';
-  import Div from '@smui/common/Div.svelte';
-  import './image-list.scss';
-
-  function getUnevenImageSize(counter, base, variance, preAdd = (num) => num) {
-    const mid = (counter % 2 ? Math.cos : Math.sin)(counter) * variance;
-    return base + Math.floor(preAdd(mid));
-  }
+  import ShowSource from '../../../components/ShowSource.svelte';
+  import Simple from './Simple.svelte';
+  import TextProtection from './TextProtection.svelte';
+  import Masonry from './Masonry.svelte';
+  import EnforceAspectRatio from './EnforceAspectRatio.svelte';
 </script>
