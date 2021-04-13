b0eb2638d260a9f7dbb5e32479870a6fd3424487
Hunter Perrin <hperrin@gmail.com>
2021-04-13 08:13:50 -0700

    chore: forgot the index file

--- site/src/routes/demo/switch/index.svelte
+++ site/src/routes/demo/switch/index.svelte
@@ -6,11 +6,18 @@
   <h2>Switch</h2>
 
   <Simple />
+  <ShowSource file="switch/Simple.svelte" />
 
   <PrimaryColor />
+  <ShowSource file="switch/PrimaryColor.svelte" />
 
   <Group />
+  <ShowSource file="switch/Group.svelte" />
 </section>
 
 <script>
+  import ShowSource from '../../../components/ShowSource.svelte';
+  import Simple from './Simple.svelte';
+  import PrimaryColor from './PrimaryColor.svelte';
+  import Group from './Group.svelte';
 </script>
