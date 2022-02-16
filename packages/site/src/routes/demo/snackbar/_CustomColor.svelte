f6b361c8ce38abc8b39fde109830af887afd7622
fcrozatier <48696601+fcrozatier@users.noreply.github.com>
2022-02-16 08:37:04 +0100

    Create a Custom Color Snackbar demo
    Documents how to customize the snackbar background color

---
+++ packages/site/src/routes/demo/snackbar/_CustomColor.svelte
@@ -0,0 +1,20 @@
+<Snackbar surface$class="bg-green" bind:this={customSnackbar}>
+	<Label>Success snackbar with custom color !</Label>
+</Snackbar>
+
+<Button on:click={() => customSnackbar.open()}>
+	<Label>Open Custom Snackbar</Label>
+</Button>
+
+<script lang="ts">
+	import Snackbar, { Label, SnackbarComponentDev } from '@smui/snackbar';
+	import Button from '@smui/button';
+
+	let customSnackbar: SnackbarComponentDev;
+</script>
+
+<style>
+	:global(.bg-green) {
+		background-color: green;
+	}
+</style>
