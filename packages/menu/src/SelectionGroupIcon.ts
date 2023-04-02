a8c33bb8e1b5f56bb2dc607b8e6610affa5f4d18
Michel Bystranowski <bystrano@gmx.ch>
2023-04-02 21:30:30 +0200

    fix ts builds for the menu component

--- packages/menu/src/SelectionGroupIcon.ts
+++ packages/menu/src/SelectionGroupIcon.ts
@@ -1,9 +1,9 @@
-import type { ComponentType } from 'svelte';
 import type { SmuiElementMap } from '@smui/common';
 import { classAdderBuilder } from '@smui/common/classadder';
 import { Graphic } from '@smui/list';
 
-export default classAdderBuilder<keyof SmuiElementMap, ComponentType<Graphic>>({
+// @ts-ignore
+export default classAdderBuilder<keyof SmuiElementMap, Graphic>({
   class: 'mdc-menu__selection-group-icon',
   component: Graphic,
 });
